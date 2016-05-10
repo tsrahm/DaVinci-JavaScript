3f3ed0fa1164eeafd9c06e9f8e8a1b4c565964fc
Torsten <torsten@AnniesMacLaptop.local>
2016-05-09 20:19:37 -0600

    Re-structured website

---
+++ client/pages/todo.js
@@ -0,0 +1,103 @@
+
+var $ = require('jquery');
+
+// Legacy loading for Bootstrap
+window.jQuery = window.$ = $;
+require('bootstrap');
+
+import _ from 'underscore';
+import Handlebars from 'handlebars';
+import lscache from 'lscache';
+
+
+
+// Array of To Dos
+var savedData = lscache.get('todos');
+var todos;
+if (savedData === null) {
+  todos = [];
+} else {
+  todos = savedData;
+}
+
+// Application
+var template;  
+var app = {
+  init: function(){
+    app.compileTemplates();
+    app.render();
+  },
+  compileTemplates: function(){
+    // Assign <script> to var template
+    template = $('[type="text/x-template"]');
+    // Pass HTML content of <script> to Handlebars compiler.
+    // Handlebars takes this and compiles to a JS function.
+    template = Handlebars.compile(template.first().html());
+  },
+  render: function(){
+    lscache.set('todos', todos);
+    // Create an array of HTML strings
+    var todoHtml = _.map(todos, function(todo){
+      // The function template returns HTML code
+      // with our data interpolated into the HTML.
+      return template(todo);
+    });
+    app.unbindEvents();
+    // .join converts the array of strings into one string
+    // .html adds the HTML string to our <ul>
+    $('ul.list-group').html(todoHtml.join(''));
+    app.bindEvents();
+  },
+  unbindEvents: function(){
+    $('.list-group-item').off();
+    $("input[type='checkbox']").off();
+    $('.add-todo-container button').off();
+    $('.list-group-item button').off();
+  },
+  bindEvents: function(){
+    app.bindHoverEvents();
+    app.bindCheckboxEvents();
+    app.bindAddTodoEvents();
+    app.bindRemoveTodoEvents();
+  },
+  bindHoverEvents: function(){
+    var $items = $('.list-group-item');
+    $items.on('mouseover', function(){
+      $(this).addClass('list-group-item-success');
+    });
+    $items.on('mouseout', function(){
+      $(this).removeClass('list-group-item-success');
+    });
+  },
+  bindCheckboxEvents: function(){
+    var $checkboxes = $("input[type='checkbox']");
+    $checkboxes.on('change', function(){
+      var isChecked = !$(this).is(':checked');
+      if (isChecked) {
+        $(this).parent().parent().removeClass('disabled');
+      } else {
+        $(this).parent().parent().addClass('disabled');
+      }
+    });
+  },
+  bindAddTodoEvents: function(){
+    $('.add-todo-container button').on('click', function(){
+      var newTodoTitle = $('.add-todo-container input').val();
+      if (_.isString(newTodoTitle) && newTodoTitle.length > 2) {
+        var newTodoObject = {title: newTodoTitle, completed: false};
+        todos.push(newTodoObject);
+        app.render();
+      }
+      $('.add-todo-container input').val('');
+    });
+  },
+  bindRemoveTodoEvents: function(){
+    $('.list-group-item button').on('click', function(){
+      var index = $(this).parent().parent().index();
+      todos.splice(index, 1);
+      app.render();
+    });
+  }
+};
+
+module.exports = app;
