67b7b50b9d1e9018cbc515117a297f3db86d6565
Torsten <torsten@AnniesMacLaptop.local>
2016-06-17 22:41:16 -0600

    updated dyba mobile view

--- client/pages/todo/todo-model.js
+++ client/pages/todo/todo-model.js
@@ -31,9 +31,19 @@ var TodoModel = Backbone.Model.extend({
     });
   },
   save: function() {
-    // var data = this.get('todos');
-    // data = this.applySchema(data);
-    // lscache.set('todos', data);
+    var that = this;
+    var todos = this.get('todos');
+    $.ajax({
+      url: '/api',
+      method: 'POST',
+      data: {todos: JSON.stringify(todos)},
+      complete: function(response) {
+        var dataString = response.responseText;
+        var data = JSON.parse(dataString);
+        data = that.applySchema(data);
+        that.set('todos', data);
+      }
+    });
   },
   applySchema: function(todos) {
     var data = todos;
